c57d948f1b5cf6a5429fa2bfc105ae9bc07752fa
callmendre <88037599+callmendre@users.noreply.github.com>
2021-07-27 20:36:24 +0800

    Add files via upload

---
+++ membuat program kasir.py
@@ -0,0 +1,22 @@
+def awal():
+    print("\n       PENYEMPURNAAN KASIR")
+    print("\nSELAMAT DATANG DIPROGRAM SEDERHANA KASIR")
+    print("\n\n             MENU MAKANAN")
+    print("\n[1]   BAKSO                       = Rp,15,000")
+    print("[2]   MIE AYAM                 = Rp.12,500")
+    print("[3]   SOTO                         = Rp.10,000")
+    print("[4]   MIE AYAM+BAKSO   = Rp.18,500")
+
+    menu1 = "BAKSO"; harga1 = 15000
+    menu2 = "MIE AYAM"; harga2 = 12500
+    menu3 = "SOTO"; harga3 = 10000
+    menu4 = "MIE AYAM+BAKSO"; harga4 = 18500
+    
+    pilih_menu = int(input("\nAnda Ingin Pilih Menu Nomor Berapa? : "))
+    print(menu1,"Rp.",harga1)
+awal()
+        
+        
+    
+                               
+                               
